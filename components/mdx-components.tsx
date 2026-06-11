bc138e0626b1d9aba10841d51f5c4b1df61c032e
QUDDUS <qudduslarek@proton.me>
2026-06-11 20:45:37 +0500

    fix: link and id for navigation target

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,48 +1,50 @@
-"use client"
+"use client";
 import { type ComponentProps } from "react";
 
 type HTMLProps = ComponentProps<"div">;
 import { Typography } from "@heroui/react";
+import { generateSlug } from "@postfolio/core";
+
 
 export const components: any = {
   h1: ({ children }: HTMLProps) => (
-    <Typography.Heading level={1}>
+    <Typography.Heading level={1} id={generateSlug(children)}>
       {children}
     </Typography.Heading>
   ),
 
   h2: ({ children }: HTMLProps) => (
-    <Typography.Heading level={2}>
+    <Typography.Heading level={2} id={generateSlug(children)}>
       {children}
     </Typography.Heading>
   ),
 
   h3: ({ children }: HTMLProps) => (
-    <Typography.Heading level={3}>
+    <Typography.Heading level={3} id={generateSlug(children)}>
       {children}
     </Typography.Heading>
   ),
 
   h4: ({ children }: HTMLProps) => (
-    <Typography.Heading level={4}>
+    <Typography.Heading level={4} id={generateSlug(children)}>
       {children}
     </Typography.Heading>
   ),
 
   h5: ({ children }: HTMLProps) => (
-    <Typography.Heading level={5}>
+    <Typography.Heading level={5} id={generateSlug(children)}>
       {children}
     </Typography.Heading>
   ),
 
   h6: ({ children }: HTMLProps) => (
-    <Typography.Heading level={6}>
+    <Typography.Heading level={6} id={generateSlug(children)}>
       {children}
     </Typography.Heading>
   ),
 
   p: ({ children }: HTMLProps) => (
-    <Typography.Paragraph size="sm">
+    <Typography.Paragraph size="sm" id={generateSlug(children)}>
       {children}
     </Typography.Paragraph>
   ),
