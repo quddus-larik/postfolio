c39a2c262e995b3f43050776442bd140786d8bc0
QUDDUS <qudduslarek@proton.me>
2026-06-12 21:41:18 +0500

    package: rename '@postfolio/core' to 'postfolio'

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ["@postfolio/core"],
+  transpilePackages: ["postfolio"],
   serverExternalPackages: ["esbuild", "mdx-bundler"],
 };
 
