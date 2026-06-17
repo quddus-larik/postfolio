fb09a84059d10e1a9a5f72437de5f1e406dcf217
QUDDUS <qudduslarek@proton.me>
2026-06-17 20:16:11 +0500

    chore: update blog UI layout

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,15 +1,12 @@
 import { components } from "@/components/mdx-components";
 import {
   Label,
-  Button,
   Description,
   Separator,
-  Chip,
   Avatar,
-  ButtonGroup,
 } from "@heroui/react";
 import { MDXPost, Slugs, generateTOC } from "postfolio/server";
-import { Content } from "postfolio/renderer"
+import { Content } from "postfolio/renderer";
 import { notFound } from "next/navigation";
 import { TableOfContents } from "@/components/table-of-contents";
 
@@ -39,7 +36,10 @@ export default async function Page({
       <div className="bg-background py-4 -mx-4 px-4 space-y-3">
         <div className="flex flex-row gap-3">
           <Avatar>
-            <Avatar.Image src={post.frontmatter["avatar"]} alt="avatar" />
+            <Avatar.Image
+              src={post.frontmatter["avatar"] as string}
+              alt="avatar"
+            />
             <Avatar.Fallback>QL</Avatar.Fallback>
           </Avatar>
           <div className="flex flex-col gap-1 justify-start">
@@ -52,30 +52,13 @@ export default async function Page({
             <Description>{post.frontmatter.date}</Description>
           </div>
         </div>
-        <div className="flex flex-row gap-1 justify-start">
-          {post.frontmatter.tags?.map((itm: any) => (
-            <Chip size="sm" key={itm} variant="primary" color="accent">
-              {itm}
-            </Chip>
-          ))}
-        </div>
-        <div className="flex flex-row gap-1 justify-between items-start">
-          <div className="flex gap-2 items-center">
-          <Button size="sm" variant="tertiary">Github</Button>
-          <Button size="sm" variant="tertiary">Twitter X</Button>
-          <Button size="sm" variant="tertiary">Instagram</Button>
-          </div>
-          <ButtonGroup variant="tertiary">
-            <Button size="sm">Copy Markdown</Button>
-            <Button size="sm">Open</Button>
-          </ButtonGroup>
-        </div>
+
         <TableOfContents toc={toc} />
       </div>
       <article className="space-y-3">
         <div className="h-32 w-full">
           <img
-            src={post.frontmatter.cover}
+            src={post.frontmatter.cover as string}
             alt={post.frontmatter.title || "Post cover"}
             className="w-full h-full object-cover rounded-3xl"
           />
@@ -88,9 +71,16 @@ export default async function Page({
         </div>
         <Content components={components} code={post.code} />
         <Separator className="my-2" />
-        <div className="flex flex-col gap-1 justify-start">
+        <div className="flex flex-row gap-2 items-center justify-start">
           <Label>Source</Label>
-          <Description>{post.frontmatter.source}</Description>
+          <a
+            href={`https://github.com/quddus-larik/postfolio/blob/main/content/blogs/${post.slug}.mdx`}
+            className="hover:underline"
+          >
+            <Description>
+              https://github.com/quddus-larik/postfolio/blob/main/content/blogs/{post.slug}.mdx
+            </Description>
+          </a>
         </div>
       </article>
     </div>
